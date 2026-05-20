958d659be3caae20e43866a412de8cbef9b5471b
hbv1317 <hv2017@gmail.com>
2026-05-20 14:45:39 -0400

    Incorporating deaths as of May 20, 2026

--- Code/ice_mortality_validation.R
+++ Code/ice_mortality_validation.R
@@ -1,7 +1,7 @@
 # Title: ICE Mortality Validation
 # Authors: Hena Vadher, Annette Dekker, and Ethan Corey for BBDP
 # Date (Last Updated): 26 February 2026
-# Date (Last Ran): 18 May 2026
+# Date (Last Ran): 20 May 2026
 # Purpose: Clean and validate ICE mortality data.
 
 library(common)
